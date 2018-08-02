4ce4726ab633217c1f14900f06cd9785642949a9
judy-chen <vaelyra@gmail.com>
2018-08-01 23:16:45 -0700

    attempt to parse html link

--- PriceFlip/MainWindow.xaml.cs
+++ PriceFlip/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using HtmlAgilityPack;
 namespace PriceFlip
 {
     /// <summary>
@@ -25,7 +27,29 @@ namespace PriceFlip
             InitializeComponent();
         }
 
+        class MyWebClient : WebClient
+        {
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                HttpWebRequest request = base.GetWebRequest(address) as HttpWebRequest;
+                request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+                return request;
+            }
+        }
+
         private void FavouritesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Url = @"http://currency.poe.trade/search?league=Incursion&online=x&want=4&have=5";
+            
+            var data = new MyWebClient().DownloadString(Url);
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(data);
+            string seventh = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[9]/div[1]")[0].InnerText;
+            Console.WriteLine(seventh);
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
         {
 
         }
