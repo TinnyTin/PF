605ad0d0ceb66729d1991d77133c1c2260555424
judy-chen <vaelyra@gmail.com>
2018-08-02 18:45:03 -0700

    Search all currencies, from currency to chaos and chaos to currency. Warning: wait up to 50 seconds.

--- PriceFlip/MainWindow.xaml.cs
+++ PriceFlip/MainWindow.xaml.cs
@@ -84,25 +84,76 @@ namespace PriceFlip
             this.DragMove();
         }
 
-        private void refresh(object sender, RoutedEventArgs e)
-        {
-            var Url = @"http://currency.poe.trade/search?league=Incursion&online=x&stock=&want=" + currency["alch"] + "&have=" + currency["chaos"];
-            var data = new MyWebClient().DownloadString(Url);
-            var doc = new HtmlDocument();
-            doc.LoadHtml(data);
+        private void refresh(object sender, RoutedEventArgs e) { 
+        //{
+        //    foreach (KeyValuePair<string, int> entry in currency)
+        //    {
+        //        if (entry.Value != 4)
+        //        {
+        //            string data;
+        //            using (var client = new WebClient())
+        //            {
+        //                string link = "http://currency.poe.trade/search?league=Incursion&online=x&stock=&want=" + entry.Value + "&have=" + currency["chaos"];
+        //                data = client.DownloadString(link);
+        //                HtmlDocument doc = new HtmlDocument();
+        //                doc.LoadHtml(data);
+        //                var htmlNode = doc.DocumentNode.SelectNodes("//div[@class='displayoffer ']")[6];
 
-            var htmlNode = doc.DocumentNode.SelectNodes("//div[@class='displayoffer ']")[6];
+        //                var ign = htmlNode.GetAttributeValue("data-ign", "IGN not found");
+        //                var receive = htmlNode.GetAttributeValue("data-sellvalue", "Sell value not found");
+        //                var pay = htmlNode.GetAttributeValue("data-buyvalue", "Buy value not found");
+        //                var stock = htmlNode.GetAttributeValue("data-stock", "Stock not found");
 
-            var ign = htmlNode.GetAttributeValue("data-ign", "IGN not found");
-            var receive = htmlNode.GetAttributeValue("data-sellvalue", "Sell value not found");
-            var pay = htmlNode.GetAttributeValue("data-buyvalue", "Buy value not found");
-            var stock = htmlNode.GetAttributeValue("data-stock", "Stock not found");
+        //                //Test print
+        //                string msg = "IGN: " + ign + ", Currency: " + entry.Key + ", Receive: " + receive + ", Pay: " + pay + ", Stock: " + stock + "\n";
+        //                stringbox.Text = msg;
+
+        //            }
+        //        }
+        //    }
+        //}
+                    foreach (KeyValuePair<string, int> entry in currency)
+                        {
+                            if (entry.Value != 4)
+                            {
+                                var Url = @"http://currency.poe.trade/search?league=Incursion&online=x&stock=&want=" + entry.Value + "&have=" + currency["chaos"];
+                                var data = new MyWebClient().DownloadString(Url);
+                                var doc = new HtmlDocument();
+                                doc.LoadHtml(data);
+
+                                var htmlNode = doc.DocumentNode.SelectNodes("//div[@class='displayoffer ']")[6];
+
+                                var ign = htmlNode.GetAttributeValue("data-ign", "IGN not found");
+                                var receive = htmlNode.GetAttributeValue("data-sellvalue", "Sell value not found");
+                                var pay = htmlNode.GetAttributeValue("data-buyvalue", "Buy value not found");
+                                var stock = htmlNode.GetAttributeValue("data-stock", "Stock not found");
 
 
-            //Test print
-            string msg = "IGN: " + ign + ", Receive: " + receive + ", Pay: " + pay + ", Stock: " + stock + "\n";
-            stringbox.Text = msg;
+                                //Test print
+                                string msg = "IGN: " + ign + ", Currency: chaos to " + entry.Key + ", Receive: " + receive + ", Pay: " + pay + ", Stock: " + stock + "\n";
+                                stringbox.Text += msg;
 
-        }
-    }
+
+                                var Urlx = @"http://currency.poe.trade/search?league=Incursion&online=x&stock=&want=" + currency["chaos"] + "&have=" + entry.Value;
+                                var datax = new MyWebClient().DownloadString(Urlx);
+                                var docx = new HtmlDocument();
+                                docx.LoadHtml(datax);
+
+                                var htmlNodex = docx.DocumentNode.SelectNodes("//div[@class='displayoffer ']")[6];
+
+                                var ignx = htmlNodex.GetAttributeValue("data-ign", "IGN not found");
+                                var receivex = htmlNodex.GetAttributeValue("data-sellvalue", "Sell value not found");
+                                var payx = htmlNodex.GetAttributeValue("data-buyvalue", "Buy value not found");
+                                var stockx = htmlNodex.GetAttributeValue("data-stock", "Stock not found");
+
+
+                                //Test print
+                                string msgx = "IGN: " + ignx + ", Currency:" + entry.Key + " to chaos" + ", Receive: " + receivex + ", Pay: " + payx + ", Stock: " + stockx + "\n";
+                                stringbox.Text += msgx;
+                }
+                        }
+                    }
+
+                }
+
 }
